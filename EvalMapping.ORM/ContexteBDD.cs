a63aeb3d686c4b82cb0778f7c90aa0de3c5dc04b
FlorianSinegre <45358364+FlorianSinegre@users.noreply.github.com>
2019-11-14 10:49:17 +0100

    Ajoutez des fichiers projet.

---
+++ EvalMapping.ORM/ContexteBDD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EvalMapping.ORM;
+
+namespace EvalMapping.ORM
+{
+    public class ContexteBDD : DbContext
+    {
+        public ContexteBDD()
+            : base("ChaineDeConnexion")
+        {
+
+        }
+
+        /*public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentGrade> StudentGrades { get; set; }
+        public DbSet<Person> persons { get; set; }
+        public DbSet<Departement> Departements { get; set; }*/
+
+
+    }
+}
---
+++ EvalMapping.ORM/exempleDEClasse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvalMapping.ORM
+{
+    public class exempleDEClasse
+    {
+            
+        /*[Key]
+        public int CourseId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<> Persons { get; set; }
+        public virtual ICollection<> StudentGrades { get; set; }
+
+        public Course()
+        {
+            Persons = new List<Person>();
+            StudentGrades = new List<StudentGrade>();
+        }*/
+    }
+}
+
